b1b22ee295cfe99466776cdea27916195aacafc5
LinSun-UCSD <chrissun1996@gmail.com>
2022-10-23 10:47:35 -0700

    add plotter

---
+++ 2DShearBuilding/plotter.py
@@ -0,0 +1,35 @@
+import _pickle as cPickle
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from seaborn import scatterplot
+from plotter.plotScatterTwoTheta import plotScatterTwoTheta
+
+
+
+# load trace
+with open('mytrace.pickle', 'rb') as handle1:
+    mytrace = pickle.load(handle1)
+# mytrace stage m:
+# samples, likelihood, weights, next stage ESS, next stage beta, resampled samples
+trueValues = [1e9, 1e9]
+stages = np.arange(20, 22)
+labelsName = ["k1", "k2"]
+pickleFileName ='mytrace.pickle'
+# plotScatterTwoTheta(pickleFileName, trueValues, stages, labelsName)
+
+# model evidence
+evidence = 1
+for i in range(len(mytrace)):
+    Wm = mytrace[i][2]
+    evidence = evidence * (sum(Wm) / len(Wm))
+
+# plot distribution
+sns.set_theme(style="white")
+g = sns.PairGrid(pd.DataFrame(mytrace[-1][0][:, :2]), diag_sharey=False)
+g.map_upper(sns.scatterplot, s=15)
+g.map_lower(sns.kdeplot)
+g.map_diag(sns.kdeplot, lw=12)
+plt.show()
---
+++ 2DShearBuilding/plotter/plotScatterTwoTheta.py
@@ -0,0 +1,39 @@
+import _pickle as cPickle
+import matplotlib.pyplot as plt
+import pickle
+
+"""
+@Author: Lin Sun
+INPUT:
+pickleFileName: picke file name
+trueValues: list of two
+stages: numpy array of stage to plot the stages scatter plot
+labelsName: list of string to name xlabel and ylabel
+"""
+
+
+def plotScatterTwoTheta(pickleFileName, trueValues, stages, labelsName):
+
+    with open(pickleFileName, 'rb') as handle1:
+        mytrace = pickle.load(handle1)
+
+    for i in stages:
+        plt.figure(figsize=(6, 5), dpi=100)
+        plt.rc('xtick', labelsize=16)
+        plt.rc('ytick', labelsize=16)
+        plt.plot(trueValues[1], trueValues[0], 'ro', label='True')
+        Sm = mytrace[i][0]
+        Wm = mytrace[i][2]
+        if i == 0:
+            betap = 0
+        else:
+            betap = mytrace[i - 1][-2]
+        plt.scatter(Sm[:, 1], Sm[:, 0], s=(Wm / sum(Wm)) * 1000, zorder=2)
+        plt.xlim([0.8 * (trueValues[1]), 1.2 * (trueValues[1])])
+        plt.ylim([0.8 * (trueValues[0]), 1.2 * (trueValues[0])])
+        plt.title(r'$ \beta_{%d} $ = %.5f' % (i, betap), fontsize=16)
+        plt.ylabel(labelsName[0], fontsize=16)
+        plt.xlabel(labelsName[1], fontsize=16)
+        plt.tight_layout()
+    plt.show()
+    plt.close('all')
