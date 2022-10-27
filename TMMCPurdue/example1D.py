c7042ac0289cb4c53fac7092e7df23330ada2d4d
LinSun-UCSD <chrissun1996@gmail.com>
2022-10-27 11:08:11 -0700

    clean

--- TMMCPurdue/example1D.py
+++ TMMCPurdue/example1D.py
@@ -1,25 +1,42 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
+
 # Pick a starting point for your random walk
 x0 = 0.
 # Pick a standard deviation for your random walk
-sigma = 0.1
+sigma = [0.1, 1]
 # Pick the number of steps you want to simulate
 n = 10000
 # How many different sample paths of the process do you want to simulate
-n_paths = 10
+n_paths = 5
 # We will be plotting in here:
-fig, ax = plt.subplots()
-# Loop over the paths
-for _ in range(n_paths):
-    # Simulate a single path
-    X = np.ndarray((n + 1,))
-    X[0] = x0
-    for t in range(1, n + 1):
-        Zt = np.random.randn()
-        X[t] = X[t-1] + sigma * Zt
-    # Let's plot it
-    ax.plot(range(n+1), X)
-ax.set_xlabel('$n$ (steps)')
-ax.set_ylabel('$X_n$');
-plt.show()
+plt.figure(figsize=(7, 6))
+for j in range(len(sigma)):
+    sig = sigma[j]
+    plt.subplot(2, 1, j + 1)
+    # Loop over the paths
+    for _ in range(n_paths):
+        # Simulate a single path
+        X = np.ndarray((n + 1,))
+        X[0] = x0
+        for t in range(1, n + 1):
+            Zt = np.random.randn()
+            X[t] = X[t - 1] + sig * Zt
+        # Let's plot it
+        plt.plot(range(n + 1), X)
+    plt.xlabel('$n$ (steps)')
+    plt.ylabel('$X_n$')
+    legenName = []
+    for i in range(n_paths):
+        legenName.append("path " + str(i + 1))
+    plt.legend(legenName, loc="upper left")
+    plt.title(r'$\sigma$' + "=" + str(sig))
+
+    plt.xlim(0, n)
+    plt.grid("auto")
+
+plt.tight_layout()
+plt.savefig("1D example.png")
+plt.show(block=True)
